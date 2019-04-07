88cd47ef7be38b3152b911416f0877d47a0a76a8
Abdullah Barnawi <ab.barnawi@gmail.com>
2019-04-07 15:36:50 +0300

    Paper.java Filled

--- src/Paper.java
+++ src/Paper.java
@@ -1,4 +1,51 @@
 
 public class Paper {
+	private String name;
+	private Note[] notes;
+	private double rating
+	
+	private String author;
+	private String date;
+	private String keywords;
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getKeywords() {
+		return keywords;
+	}
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Note[] getNotes() {
+		return notes;
+	}
+	public void setNotes(Note[] notes) {
+		this.notes = notes;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	
+	
+	
 
 }
