e53a573f26d5a461e6b4b924f15984b0d28934ef
Saud Aljaffer <saud.aljaffer@gmail.com>
2019-04-07 15:46:36 +0300

    fixed errors

--- src/Note.java
+++ src/Note.java
@@ -1,14 +1,37 @@
 import java.util.ArrayList;
 
 public class Note {
-String title;
-String category;
-String content;
+	private String title;
+	private String category;
+	private String content;
 
+	public ArrayList<String> getKeywords() {
+		// TODO Auto-generated method stub
 
-public ArrayList<String> getKeywords() {
-	// TODO Auto-generated method stub
+		return null;
+	}
 
-	return null;
-}
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
