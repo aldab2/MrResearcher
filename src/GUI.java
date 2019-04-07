2a0da05be93c25657969930b4e047ef1ad145daf
YounesB <younesbcs@gmail.com>
2019-04-07 15:53:16 +0300

    GUI Class

---
+++ src/GUI.java
@@ -0,0 +1,55 @@
+package System;
+
+
+/**
+ * @author YounesB
+ * @version 1.0
+ * @created 07-Apr-2019 3:52:23 PM
+ */
+public class GUI {
+
+	private PaperBuilder paperBuilder;
+
+	public GUI(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void addNote(){
+
+	}
+
+	public void addPaper(){
+
+	}
+
+	public void deleteNote(){
+
+	}
+
+	public void deletePaper(){
+
+	}
+
+	public void editNote(){
+
+	}
+
+	public void genRelationShipDiagram(){
+
+	}
+
+	public void giveRating(){
+
+	}
+
+	public Note searchNotes(){
+		return null;
+	}
+
+	public Paper searchPaper(){
+		return null;
+	}
+}//end GUI
