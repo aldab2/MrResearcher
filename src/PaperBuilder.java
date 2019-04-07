c7d41b8efbdcd4e805eb2385f1b2540fcb39bc5f
alialali <alooy.ali40@gmail.com>
2019-04-07 16:00:06 +0300

    Paper builder
    class

---
+++ src/PaperBuilder.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+public class PaperBuilder {
+	private Paper tmp;
+	private File file;
+	
+	public Paper buildPaper(File file) {
+		return tmp;
+	}
+	private boolean verifyFile() {
+		return false;
+	}
+	private void RenameFile() {
+
+	}
+	private void saveFile() {
+	}
+
+}
