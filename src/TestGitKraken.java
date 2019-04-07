94f3d2d38570b04b9530380d85600a5083af0411
Saud Aljaffer <saud.aljaffer@gmail.com>
2019-04-07 15:22:07 +0300

    Resolving conflict

--- src/TestGitKraken.java
+++ src/TestGitKraken.java
@@ -4,9 +4,8 @@ public class TestGitKraken {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
-       
+		 System.out.println("koko");
         System.out.println("no");
-
 	}
 	
 }
