ff57ec889570f3a2808cf54ec9bc91c215127e48
Unknown <ab.barnawi@gmail.com>
2019-04-09 15:32:36 +0300

    VerifyFile

--- src/PaperBuilder.java
+++ src/PaperBuilder.java
@@ -1,14 +1,40 @@
 import java.io.File;
 
+
 public class PaperBuilder {
 	private Paper tmp;
 	private File file;
+	final static String PAPERS_DIRECTORY="papers/";
+	
+	public static void main(String[] args) {
+		
+	
+	}
 
 	public Paper buildPaper(File file) {
 		return tmp;
 	}
 
-	private boolean verifyFile() {
+	private static boolean verifyFile(File file) {
+		System.out.println("Verfying File : "+file.getAbsolutePath()+"  "+file.exists());
+
+		String extension = "";
+		 
+        try {
+            if (file != null /*&& file.exists()*/) {
+                String name = file.getName();
+                extension = name.substring(name.lastIndexOf("."));
+                System.out.println("HERE");
+            }
+        } catch (Exception e) {
+            extension = "";
+        }
+        
+        System.out.println("Extension is : "+extension);
+        if(extension.equals(".pdf")) {
+        	return true;
+		
+        }
 		return false;
 	}
 
