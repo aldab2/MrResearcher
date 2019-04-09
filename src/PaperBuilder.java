21180d0981700e3a3475bce6c75a55c4ffe1b575
Saud <saud.aljaffer@gmail.com>
2019-04-09 17:46:14 +0300

    Fixed

--- src/PaperBuilder.java
+++ src/PaperBuilder.java
@@ -3,12 +3,36 @@ import java.io.File;
 public class PaperBuilder {
 	private Paper tmp;
 	private File file;
+	final static String PAPERS_DIRECTORY = "papers/";
 
 	public Paper buildPaper(File file) {
+		// verifyFile();
+		saveFile();
+		RenameFile();
+
 		return tmp;
 	}
 
 	private boolean verifyFile() {
+		System.out.println("Verfying File : " + file.getAbsolutePath() + "  " + file.exists());
+
+		String extension = "";
+
+		try {
+			if (file != null /* && file.exists() */) {
+				String name = file.getName();
+				extension = name.substring(name.lastIndexOf("."));
+				System.out.println("HERE");
+			}
+		} catch (Exception e) {
+			extension = "";
+		}
+
+		System.out.println("Extension is : " + extension);
+		if (extension.equals(".pdf")) {
+			return true;
+
+		}
 		return false;
 	}
 
