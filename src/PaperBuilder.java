be8187ebbb9095dd4e96848de5ff07f23eae3743
Saud <saud.aljaffer@gmail.com>
2019-04-09 16:06:05 +0300

    issue fix

--- src/PaperBuilder.java
+++ src/PaperBuilder.java
@@ -1,14 +1,12 @@
 import java.io.File;
 
-
 public class PaperBuilder {
 	private Paper tmp;
 	private File file;
-	final static String PAPERS_DIRECTORY="papers/";
-	
+	final static String PAPERS_DIRECTORY = "papers/";
+
 	public static void main(String[] args) {
-		
-	
+
 	}
 
 	public Paper buildPaper(File file) {
@@ -16,25 +14,25 @@ public class PaperBuilder {
 	}
 
 	private static boolean verifyFile(File file) {
-		System.out.println("Verfying File : "+file.getAbsolutePath()+"  "+file.exists());
+		System.out.println("Verfying File : " + file.getAbsolutePath() + "  " + file.exists());
 
 		String extension = "";
-		 
-        try {
-            if (file != null /*&& file.exists()*/) {
-                String name = file.getName();
-                extension = name.substring(name.lastIndexOf("."));
-                System.out.println("HERE");
-            }
-        } catch (Exception e) {
-            extension = "";
-        }
-        
-        System.out.println("Extension is : "+extension);
-        if(extension.equals(".pdf")) {
-        	return true;
-		
-        }
+
+		try {
+			if (file != null /* && file.exists() */) {
+				String name = file.getName();
+				extension = name.substring(name.lastIndexOf("."));
+				System.out.println("HERE");
+			}
+		} catch (Exception e) {
+			extension = "";
+		}
+
+		System.out.println("Extension is : " + extension);
+		if (extension.equals(".pdf")) {
+			return true;
+
+		}
 		return false;
 	}
 
